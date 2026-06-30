1021e38a7f7bf9bd013b0b94240ddc6622dee065
Shadhau <bonzanini.ryan@gmail.com>
2026-06-30 16:47:53 +0200

    update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Autorise les connexions externes (comme celle du tunnel Cloudflare)
+    port: 5174,
+  }
 })
